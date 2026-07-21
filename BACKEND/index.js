c3545df14aee80c761ab73ea66ca6ff4a3568dfa
Souvik Kundu <iamkundusouvik@gmail.com>
2026-07-21 16:10:08 +0530

    fix: update CORS configuration to use dynamic LOCAL_URL

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -18,7 +18,7 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            "http://localhost:5173",
+            import.meta.env.LOCAL_URL,
             import.meta.env.VITE_FRONTEND_URL,
         ],
         methods: "GET,POST,PUT,DELETE,PATCH",
